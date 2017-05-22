93289173f0e284eff7eb62afe2c9162639451fe6
Shav <shavenchew@yahoo.com>
2017-05-21 23:46:31 -0700

    Component Skeletons Added with routing ready to go

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,42 @@
+
+import { MovieDataService } from './Services/moviedata.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieRowComponent } from './movies/movie-row/movie-row.component';
+
+const appRoutes: Routes = [
+ 
+    { path : '', redirectTo: '/movies', pathMatch: 'full'},
+    { path : 'movies', component: MovieListComponent}, 
+    { path : 'hello', component: HeaderComponent}, 
+    
+    
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LatestComponent,
+    UpcomingComponent,
+    MovieListComponent,
+    MovieRowComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [MovieDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
