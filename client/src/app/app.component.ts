0c45d1ea2f3dd4b3c42b3e4d71bf0358224f8d56
Shav <shavenchew@yahoo.com>
2017-05-21 23:38:28 -0700

    app component setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+
+  constructor( private http: Http){
+
+  }
+  title = 'Welcome';
+
+  ngOnInit(){
+    
+  }
 }
