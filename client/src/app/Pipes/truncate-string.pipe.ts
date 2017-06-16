60f1a08d5ee1b2d13d87bdaee159068d66d8d72e
Shav <shavenchew@yahoo.com>
2017-06-15 22:23:51 -0700

    red text on title. Max height on cast-name

--- client/src/app/Pipes/truncate-string.pipe.ts
+++ client/src/app/Pipes/truncate-string.pipe.ts
@@ -6,8 +6,8 @@ export class TruncateStringPipe implements PipeTransform {
 
     transform(value: string, howMany: number) {
 
-        if (value.length >= 600) {
-            return value.substring(0, 600) + "...";
+        if (value.length >= 500) {
+            return value.substring(0, 500) + "...";
          } else {
              return value;
          }
